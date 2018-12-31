c7c73dbf3cf4da76f222e675525d774fb7958973
hillary-joyce <hillary.j.scofield@gmail.com>
2018-12-31 18:36:12 -0500

    updated cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000'
+    origins 'http://dog-countdown-calendar.surge.sh/'
 
     resource '*',
       headers: :any,
